94584c64a9bedeeabd7c31c9d9f6c1484f63fb0c
scanner-jiangbiao <903887001@qq.com>
2018-10-16 12:03:34 +0800

    调查实体类及基本模板创建

---
+++ investigation-parent/jdyy-investigation-entities/src/main/java/com/kalix/jdyy/investigation/entities/KneeBean.java
@@ -0,0 +1,11 @@
+package com.kalix.jdyy.investigation.entities;
+
+import com.kalix.framework.core.api.persistence.PersistentEntity;
+
+/**
+ * @author jiangbiao
+ * @create 2018-10-16 上午 11:24
+ * @desc 全膝关节调查实体类
+ **/
+public class KneeBean extends PersistentEntity {
+}
