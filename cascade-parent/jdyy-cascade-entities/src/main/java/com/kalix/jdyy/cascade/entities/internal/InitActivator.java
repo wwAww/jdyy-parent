f166f7256a3a59f98feddde1b78697aaac317b65
scanner-jiangbiao <903887001@qq.com>
2018-10-18 09:17:37 +0800

    删除cascader-parent模块

--- cascade-parent/jdyy-cascade-entities/src/main/java/com/kalix/jdyy/cascade/entities/internal/InitActivator.java
+++
@@ -1,9 +0,0 @@
-package com.kalix.jdyy.cascade.entities.internal;
-
-import com.kalix.framework.core.api.osgi.BaseBundleActivator;
-
-/**
- * @author chenyanxu
- */
-public class InitActivator extends BaseBundleActivator {
-}
