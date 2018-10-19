369019d5911d0d3631c23cd97ce3c9c957e427c9
scanner-jiangbiao <903887001@qq.com>
2018-10-19 17:44:41 +0800

    1.添加获取病员信息方法（以el-select选择起所需数据格式） 2.修改全髋、全膝术后调查实体类

--- patients-parent/jdyy-patients-api/src/main/java/com/kalix/jdyy/patients/api/biz/IPatientsBeanService.java
+++ patients-parent/jdyy-patients-api/src/main/java/com/kalix/jdyy/patients/api/biz/IPatientsBeanService.java
@@ -2,7 +2,10 @@ package com.kalix.jdyy.patients.api.biz;
 
 
 import com.kalix.framework.core.api.biz.IBizService;
+import com.kalix.framework.core.api.persistence.JsonData;
 import com.kalix.jdyy.patients.entities.PatientsBean;
 
 public interface IPatientsBeanService extends IBizService<PatientsBean> {
+//    获取病员信息（以el-select选择器需要的形式返回数据）
+    JsonData getDataBySelect();
 }
--- patients-parent/jdyy-patients-biz/src/main/java/com/kalix/jdyy/patients/biz/PatientsBeanServiceImpl.java
+++ patients-parent/jdyy-patients-biz/src/main/java/com/kalix/jdyy/patients/biz/PatientsBeanServiceImpl.java
@@ -1,10 +1,34 @@
 package com.kalix.jdyy.patients.biz;
 
+import com.kalix.framework.core.api.persistence.JsonData;
 import com.kalix.framework.core.impl.biz.GenericBizServiceImpl;
 import com.kalix.jdyy.patients.api.biz.IPatientsBeanService;
 import com.kalix.jdyy.patients.api.dao.IPatientsBeanDao;
+import com.kalix.jdyy.patients.api.dto.SelectDTO;
 import com.kalix.jdyy.patients.entities.PatientsBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PatientsBeanServiceImpl extends GenericBizServiceImpl<IPatientsBeanDao,PatientsBean>
 implements IPatientsBeanService {
+    /**
+     *
+     * @return获取病员信息（以el-select选择器需要的形式返回数据）
+     */
+    @Override
+    public JsonData getDataBySelect() {
+        List<PatientsBean> plist = dao.find("select p from PatientsBean p");
+        List<SelectDTO> list = new ArrayList<>();
+        for (PatientsBean pb : plist) {
+            SelectDTO sd = new SelectDTO();
+            sd.setLabel(pb.getName());
+            sd.setValue(pb.getId());
+            list.add(sd);
+        }
+        JsonData jsondata = new JsonData();
+        jsondata.setData(list);
+        jsondata.setTotalCount((long)list.size());
+        return jsondata;
+    }
 }
